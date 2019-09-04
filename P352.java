38c0b4e81173656677188dfc30a74c0b8722bb47
JinsuKim <jinsu6688@gmail.com>
2019-09-05 00:31:38 +0900

    p356 code  공유입니다.

---
+++ P352.java
@@ -0,0 +1,15 @@
+
+/**
+ * P352
+ *
+ * @author (JinsuKim)
+ * @version (2019.09.04)
+ */
+public class P352
+{
+    public static void main(String[] args){
+        int i = Integer.parseInt("123");
+        boolean b = Boolean.parseBoolean("true");
+        double d = Double.parseDouble("3.14");
+    }
+}
---
+++ P356.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class P356 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class P356{
+    public static void main(String[] args){
+        String a = "Hello";
+        String b = "Java";
+        String c = "Hello";
+        String d = new String("Hello");
+        String e = new String("Java");
+        String f = new String("Hello");
+        
+    }
+    }
